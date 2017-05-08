9bbb9e9f366673c7de20a250feb142bb6454ca48
Keuthonymous <mikie.daughtrey@gmail.com>
2017-05-08 13:18:53 +0200

    First example commit

---
+++ PluralSightLINQ/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PluralSightLINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] instructors = { "Aaron", "Fritz", "Scott", "Keith" };
+
+            IEnumerable<string> query = from s in instructors
+                                        where s.Length == 5
+                                        orderby s descending
+                                        select s;
+            foreach (string name in query)
+            {
+                Console.WriteLine(name);
+            }
+            Console.ReadKey();
+        }
+    }
+}
