bb6b050413ee79473d3d757d34d656c39b472b20
Keuthonymous <mikie.daughtrey@gmail.com>
2017-05-08 13:30:18 +0200

    Second Example

--- PluralSightLINQ/Program.cs
+++ PluralSightLINQ/Program.cs
@@ -10,7 +10,7 @@ namespace PluralSightLINQ
     {
         static void Main(string[] args)
         {
-            //First query example
+            /*First query example
             string[] instructors = { "Aaron", "Fritz", "Scott", "Keith" };
 
             IEnumerable<string> query = from s in instructors
@@ -22,7 +22,27 @@ namespace PluralSightLINQ
                 Console.WriteLine(name);
             }
             Console.ReadKey();
-            //End of example
+            End of example*/
+            
+            //Second example
+            IEnumerable<Employee> employees = new List<Employee>();
+            {
+                new Employee { ID = 1, Name = "Scott", HireDate = new DateTime(2002, 3, 5) };
+                new Employee { ID = 2, Name = "Poonam", HireDate = new DateTime(2002, 10, 15) };
+                new Employee { ID = 3, Name = "Paul", HireDate = new DateTime(2007, 10, 11) };
+            };
+
+            IEnumerable<Employee> query =
+                from e in employees
+                where e.HireDate.Year < 2005
+                orderby e.Name
+                select e;
+
+            foreach (Employee e in query)
+            {
+                Console.WriteLine(e.Name);
+            }
+            Console.ReadKey();
         }
     }
 }
